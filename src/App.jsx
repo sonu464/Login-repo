753788841d64c3dfaa2e60b2ec26afdf00240334
sonu464 <122866843+sonu464@users.noreply.github.com>
2023-07-23 08:50:18 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import InvalidInput from "./components/InvalidInput/InvalidInput";
 import ShowData from "./components/ShowData/ShowData";
 
 function App() {
-  const data = [{ name: "Sonu Saini", age: "23" }];
+  const data = [{ name: "Sonu Saini", age: "22" }];
   const [userData, setUserData] = useState(data);
 
   const addUserHandler = (userInput) => {
